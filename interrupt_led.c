be513729974671240b5ee709ee997df9f22b5c69
rahul-k2442 <rahulkaliyappan24@gmail.com>
2025-05-05 21:53:26 +0530

    interrupt changed to portA 2nd pin

--- interrupt_led.c
+++ interrupt_led.c
@@ -3,9 +3,9 @@ void delay(unsigned int cnt)
 {
 	while(cnt--);
 }
-void EXTI0_IRQHandler()
+void EXTI1_IRQHandler()
 {
-	if(EXTI->PR&1<<0)
+	if(EXTI->PR&1<<1)
 	{
 		GPIOB->BRR=1<<0;
 		GPIOB->ODR=1<<1;
@@ -13,20 +13,20 @@ void EXTI0_IRQHandler()
 		GPIOB->BRR=1<<1;
 		delay(500000);
 	}
-	EXTI->PR=1<<0;
+	EXTI->PR=1<<1;
 }
 int main()
 {
 	RCC->CR=0X00000083;
 	RCC->CFGR=0X00000000;
 	RCC->APB2ENR=1<<0|1<<2|1<<3;
-	GPIOA->CRL=0X00000008;
+	GPIOA->CRL=0X00000080;
 	GPIOB->CRL=0X00000022;
-	AFIO->EXTICR[0]=0X00000000;
+	AFIO->EXTICR[0]=0X00000001;
 	EXTI->IMR=1<<0;
 	EXTI->RTSR=1<<0;
 	EXTI->FTSR=0;
-	NVIC_EnableIRQ(EXTI0_IRQn);//ENABLING THE INTERRUPT
+	NVIC_EnableIRQ(EXTI1_IRQn);//ENABLING THE INTERRUPT
 	
 	while(1)
 	{
